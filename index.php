27ec0ebb0b51b497893cc890011a4b7aa591db03
Rahul <rahul007git@outlook.com>
2020-09-01 14:56:53 +0530

    rahul

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
-<?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class phone extends Model
-{
-    protected $fillable = [];
-
-
-    public function user(){
-       return $this->belongsTo[User::class];
-    }
-
-}
+test
